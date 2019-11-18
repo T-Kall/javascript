c53e81fe8d33159a283514f2ff022a80f8f22077
Tina Källklint <t.kallklint@gmail.com>
2019-11-18 13:45:57 +0100

    Lektion 1

---
+++ lektion 1/console/script.js
@@ -0,0 +1,36 @@
+// Kommentar rad
+/* Kommentar stycke */
+
+/* 
+* Märkspråk - HTML, CSS, XML   
+* Scriptspråk - Javascript, Powershell, cmdLets (kommandotolken), (Python)
+* Progammeringsspråk - C, C++, C#, Java, (Python)
+
+Java är inte JavaScript
+
+Högnivå språk (mellan användaren och systemet)
+------------------------------------------------------
+(JavaScript)
+Python
+C# / Java
+Delphi
+
+Lågnivå språk (mellan systemet och hårdvara)
+------------------------------------------------------
+C++
+C
+Cobol
+Assembler
+Mikrokod
+*/
+
+// Skriver ut på själva hemsidan
+document.body.innerText = 'Detta är en text';
+
+// Skriver ut på konsolen
+console.log('Här skriver vi nåt i konsolen');
+console.warn('Detta är en varning');
+console.error('Detta är ett felmeddelande');
+
+// SANDBOX - DEVELOPER - Här loggar vi allt!
+// PRODUCTION - SKARPT LÄGE - Här loggar vi väldigt lite!
