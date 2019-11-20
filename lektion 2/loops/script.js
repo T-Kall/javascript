33976ee0cf360c03b6b16eb1c5343289a9c28034
Tina Källklint <t.kallklint@gmail.com>
2019-11-20 09:00:34 +0100

    Lektion 2

---
+++ lektion 2/loops/script.js
@@ -0,0 +1,11 @@
+const nameArray = ['Hans','Isabelle','Gustav','Amanda','Ali','Sasha','Fredrik','Mali'];
+const userArray = [
+    { name: 'Hans', email: 'hans@hans.com', age: 35, status: true },
+    { name: 'Isabelle', email: 'isabelle@domain.com', age: 28, status: true },
+    { name: 'Gustav', email: 'gustav@domain.com', age: 31, status: false },
+    { name: 'Amanda', email: 'amanda@domain.com', age: 42, status: true },
+    { name: 'Ali', email: 'ali@domain.com', age: 23, status: true },
+    { name: 'Sasha', email: 'sasha@domain.com', age: 44, status: true },
+    { name: 'Fredrik', email: 'fredrik@domain.com', age: 53, status: true },
+    { name: 'Mali', email: 'mali@domain.com', age: 42, status: false }
+];
