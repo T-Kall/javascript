91b0172d69646713f371327e6da2ff2f0b406acb
Tina Källklint <t.kallklint@gmail.com>
2019-11-25 12:46:40 +0100

    Lektion 3

---
+++ lektion 3/selectors/script.js
@@ -0,0 +1,68 @@
+/*
+//För att kunna köra jquery lägg in detta:
+
+$(Document).ready(function() {
+
+}); 
+
+------------
+eller detta:
+
+//Samma som ovan
+
+$(function() {
+
+}); 
+*/
+
+
+
+$(function() {
+
+    /*
+    $('#btnClick1').click(function() {
+        console.log('Tryckte på knappen');
+    });
+
+    $('#btnClick1').click(() => {
+        console.log('Tryckte på knappen');
+    });
+    
+
+    $('#btnClick1').click(() => console.log('Tryckte på knappen'););
+
+
+    $("#btnClick1").click(() => {
+        $('#content').toggle();
+    });
+   
+
+    $("#btnClick1").click(() => {
+        $('#content').css('font-size', '3rem');
+    });
+
+    
+
+    $('#btnClick1').click(function() {
+        console.log(this);
+        $(this).hide();
+    });
+
+    
+
+     function hideContent() {
+         $(this).hide();
+     }
+
+    //$('p').click(hideContent();
+
+    //dollatecken är jquery
+ */
+
+
+$('ul li:odd').css('color', 'pink')
+
+
+
+ 
+});
